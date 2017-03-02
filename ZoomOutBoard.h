7151529f773e9ba7e52e899685d7603ad0bbeede
Og Astorga <og.asdiaz@gmail.com>
2017-03-02 16:50:25 -0600

    Function that calculates the zoome out board aka. grand board

---
+++ ZoomOutBoard.h
@@ -0,0 +1,11 @@
+#ifndef ZOOMOUTBOARD_H_INCLUDED
+#define ZOOMOUTBOARD_H_INCLUDED
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector<string> ZoomOutBoard(vector<string> board);
+
+#endif
