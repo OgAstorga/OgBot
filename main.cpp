16d9845d888e4051ee5cdc337632e1715fc3d8c3
Og Astorga <og.asdiaz@gmail.com>
2017-03-02 22:12:25 -0600

    Choose best move based on monte carlo

--- main.cpp
+++ main.cpp
@@ -1,15 +1,36 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "MoveWithMonteCarlo.h"
 
 using namespace std;
 
-pair<int, int> lastMove;
-string inputBoard[9];
-
 int main() {
   // Read input
+  pair<int, int> lastMove;
+  vector<string> board(9);
+
   cin >> lastMove.first >> lastMove.second;
   for (int i=0; i<9; ++i) {
-    cin >> inputBoard[i];
+    cin >> board[i];
+  }
+
+  // Calc move
+  pair<int, int> move = MoveWithMonteCarlo(board, lastMove);
+
+  // Do move
+  char player = FIRST_PLAYER;
+  if (lastMove.first != -1 && lastMove.second != -1) {
+    player = (board[lastMove.first][lastMove.second] == 'o') ? 'x' : 'o';
+  }
+
+  board[move.first][move.second] = player;
+
+  // Show output
+  cout << move.first << ' ' << move.second << endl;
+  for (auto it : board) {
+    cout << it << endl;
   }
 
   return 0;
